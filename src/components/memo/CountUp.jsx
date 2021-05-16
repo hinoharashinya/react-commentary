fbf5a7100efc3e3ea49449d69d1698b8dd99fe40
hinoharashinya <hinoshin0817@yahoo.co.jp>
2021-05-16 10:33:58 +0900

    modify child countup

--- src/components/memo/CountUp.jsx
+++ src/components/memo/CountUp.jsx
@@ -1,11 +1,10 @@
 import {useState, memo} from "react";
 
-const Child = memo((props) => {
+const Child = memo(() => {
   console.log("child is called");
   return(
     <div>
       <h3>child</h3>
-      <p>{props.count}</p>
     </div>
   );
 });
@@ -13,12 +12,18 @@ const Child = memo((props) => {
 export const CountUp = () => {
   const [count, setCount] = useState(0);
   const [childCount, setChildCount] = useState(0);
+
+  const onClickChildCount = () => {
+    setChildCount(childCount + 1);
+  }
+
   return(
     <>
       <h2>count up</h2>
       <button onClick={() => setCount(count + 1)}>count up!</button>
       <p>{count}</p>
       <Child count={childCount}/>
+      <p>{childCount}</p>
     </>
   )
 }
