dbdb77d10f5b6ad89e3b1594a7fba6f9d32541b8
hinoharashinya <hinoshin0817@yahoo.co.jp>
2021-05-16 10:53:42 +0900

    set usememo

--- src/components/memo/CountUp.jsx
+++ src/components/memo/CountUp.jsx
@@ -1,10 +1,11 @@
-import {useState, memo} from "react";
+import {useState, memo, useCallback, useMemo} from "react";
 
-const Child = memo(() => {
+const Child = memo((props) => {
   console.log("child is called");
   return(
     <div>
       <h3>child</h3>
+      <button onClick={props.onClick}>child count up!</button>
     </div>
   );
 });
@@ -13,16 +14,24 @@ export const CountUp = () => {
   const [count, setCount] = useState(0);
   const [childCount, setChildCount] = useState(0);
 
-  const onClickChildCount = () => {
+  const onClickChildCount = useCallback(() => {
     setChildCount(childCount + 1);
+  },[childCount]);
+
+  const dubleCount = (count) => {
+    console.log("call dublecount")
+    return(count * 2);
   }
+
+  const dubledCount = useMemo(() => dubleCount(count), [count]);
 
   return(
     <>
       <h2>count up</h2>
       <button onClick={() => setCount(count + 1)}>count up!</button>
       <p>{count}</p>
-      <Child count={childCount}/>
+      <p>{dubledCount}</p>
+      <Child onClick={onClickChildCount}/>
       <p>{childCount}</p>
     </>
   )
