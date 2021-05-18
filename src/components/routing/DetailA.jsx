5747bebfead31e04b8b782afb7d3e92a2f47e424
hinoharashinya <hinoshin0817@yahoo.co.jp>
2021-05-19 07:33:21 +0900

    set state

--- src/components/routing/DetailA.jsx
+++ src/components/routing/DetailA.jsx
@@ -1,7 +1,11 @@
+import {useLocation} from "react-router-dom";
+
 export const DetailA = () => {
+  const {state} = useLocation();
   return(
     <>
       <h2>DetailA</h2>
+      <p>{state}</p>
     </>
   );
 }
--- src/components/routing/Page1.jsx
+++ src/components/routing/Page1.jsx
@@ -1,9 +1,10 @@
 import {Link} from "react-router-dom";
 
 export const Page1 = () => {
+  const stateA = "state";
   return(
     <>
-      <Link to="/page1/detailA">DetailA</Link>
+      <Link to={{pathname: "/page1/detailA", state: stateA}}>DetailA</Link>
       <br />
       <Link to="/page1/detailB">DetailB</Link>
       <h2>Page1</h2>
